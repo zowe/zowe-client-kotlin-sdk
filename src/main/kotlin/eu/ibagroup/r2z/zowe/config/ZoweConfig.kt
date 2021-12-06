dfd78568db17d8af6be1f813da472ffe516b6ea8
Valiantsin Krus <76202482+hunts396@users.noreply.github.com>
2021-12-06 12:45:20 +0300

    IJMP-294: all profiles are extended by base profile now.

--- src/main/kotlin/eu/ibagroup/r2z/zowe/config/ZoweConfig.kt
+++ src/main/kotlin/eu/ibagroup/r2z/zowe/config/ZoweConfig.kt
@@ -11,6 +11,7 @@
 package eu.ibagroup.r2z.zowe.config
 
 import com.google.gson.annotations.SerializedName
+import eu.ibagroup.r2z.CodePage
 
 class ZoweConfig(
   @SerializedName("\$schema")
@@ -18,6 +19,49 @@ class ZoweConfig(
   val profiles: Map<String, ZoweConfigProfile>,
   val defaults: Map<String, String>
 ) {
+
+  inner class PropertyBuilder(val propName: String) {
+    var profilesToSearchProp = mutableListOf<ZoweConfigProfile?>()
+    fun zosmf() {
+      profilesToSearchProp.add(zosmfProfile)
+    }
+    fun tso() {
+      profilesToSearchProp.add(tsoProfile)
+    }
+    fun ssh () {
+      profilesToSearchProp.add(sshProfile)
+    }
+    fun base () {
+      profilesToSearchProp.add(baseProfile)
+    }
+  }
+
+  private fun PropertyBuilder.search (): Any? {
+    val profiles = profilesToSearchProp.filterNotNull()
+    for (profile in profiles) {
+      return profile.properties[propName] ?: continue
+    }
+    return null
+  }
+
+  fun PropertyBuilder.set (value: Any?) {
+    val profiles = profilesToSearchProp.filterNotNull()
+    for (profile in profiles) {
+      if (!profile.properties.containsKey(propName)) {
+        continue
+      }
+      profile.properties[propName] = value
+    }
+  }
+
+  fun searchProperty (propName: String, block: PropertyBuilder.() -> Unit): Any? {
+    return PropertyBuilder(propName).apply(block).search()
+  }
+
+  fun updateProperty (propName: String, propValue: Any?, block: PropertyBuilder.() -> Unit) {
+    PropertyBuilder(propName).apply(block).set(propValue)
+  }
+
   var user: String?
     get() = baseProfile?.secure?.get(0)
     set(el) { baseProfile?.secure?.set(0, el ?: "") }
@@ -27,32 +71,36 @@ class ZoweConfig(
     set(el) { baseProfile?.secure?.set(1, el ?: "") }
 
   var host: String?
-    get() = baseProfile?.properties?.get("host") as String?
-    set(el) { baseProfile?.properties?.set("host", el) }
+    get() = searchProperty("host") { zosmf(); base() } as String?
+    set(el) { updateProperty("host", el) { zosmf(); base() } }
 
   var rejectUnauthorized: Boolean?
-    get() = baseProfile?.properties?.get("rejectUnauthorized") as Boolean?
-    set(el) { baseProfile?.properties?.set("rejectUnauthorized", el ?: true) }
+    get() = searchProperty("rejectUnauthorized") { zosmf(); base() } as Boolean?
+    set(el) { updateProperty("rejectUnauthorized", el ?: true) { zosmf(); base() } }
 
   var port: Long?
-    get() = zosmfProfile?.properties?.get("port") as Long?
-    set(el) { zosmfProfile?.properties?.set("port", el) }
+    get() = searchProperty("port") { zosmf(); base() } as Long?
+    set(el) { updateProperty("port", el) { zosmf(); base() } }
 
   var protocol: String
-    get() = zosmfProfile?.properties?.get("protocol") as String? ?: "http"
-    set(el) { zosmfProfile?.properties?.set("protocol", el) }
+    get() = searchProperty("protocol") { zosmf(); base() } as String? ?: "http"
+    set(el) { updateProperty("protocol", el) { zosmf(); base() } }
 
   var basePath: String
-    get() = zosmfProfile?.properties?.get("basePath") as String? ?: "/"
-    set(el) { zosmfProfile?.properties?.set("basePath", el) }
+    get() = searchProperty("basePath") { zosmf(); base() } as String? ?: "/"
+    set(el) { updateProperty("protocol", el) { zosmf(); base() } }
 
   var encoding: Long
-    get() = zosmfProfile?.properties?.get("encoding") as Long? ?: 1047
-    set(el) { zosmfProfile?.properties?.set("encoding", el) }
+    get() = searchProperty("encoding") { zosmf(); base() } as Long? ?: 1047
+    set(el) { updateProperty("encoding", el) { zosmf(); base() } }
 
   var responseTimeout: Long
-    get() = zosmfProfile?.properties?.get("responseTimeout") as Long? ?: 600
-    set(el) { zosmfProfile?.properties?.set("responseTimeout", el) }
+    get() = searchProperty("responseTimeout") { zosmf(); base() } as Long? ?: 600
+    set(el) { updateProperty("responseTimeout", el) { zosmf(); base() } }
+
+  var codePage: CodePage
+    get() = CodePage.valueOf("IBM_${searchProperty("codePage") { tso(); base() } }")
+    set(el) { updateProperty("codePage", el.codePage.split("IBM_").last()) { tso(); base() } }
 
   val zosmfProfile: ZoweConfigProfile?
     get() = profiles[defaults["zosmf"]]
